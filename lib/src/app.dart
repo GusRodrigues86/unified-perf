cc7359dd4d212723963e7cf1b6e9cca5b2ee6405
GusRodrigues <omykronbr@gmail.com>
2023-12-14 17:02:51 -0500

    no debug banner

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -12,7 +12,7 @@ class UnifiedPerfApp extends ConsumerWidget {
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ref.watch(themeProvider),
       routerConfig: router,

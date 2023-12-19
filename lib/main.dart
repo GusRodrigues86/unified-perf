75e3c4974975e474dd11df8e247de74e2fd0bbaf
GusRodrigues <omykronbr@gmail.com>
2023-12-19 17:42:22 -0500

    implementation of min screen size.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'src/app.dart';
 import 'utils/provider_logger.dart';
 
-void main() {
-  runApp(ProviderScope(
-    observers: [ProviderLogger()],
-    child: const UnifiedPerfApp(),
-  ));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  final isDesktop = Platform.isLinux || Platform.isMacOS || Platform.isWindows;
+
+  if (isDesktop) {
+    await WindowManager.instance.setMinimumSize(const Size(640, 480));
+  }
+
+  const UnifiedPerfApp app = UnifiedPerfApp();
+  runApp(
+    ProviderScope(
+      observers: kDebugMode ? [ProviderLogger()] : null,
+      child: kDebugMode ? const UnifiedPerfApp() : app,
+    ),
+  );
 }
